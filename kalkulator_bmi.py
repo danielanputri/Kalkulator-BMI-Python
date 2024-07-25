76effcd321e0e92e7d7c40da2985acf5a9694a41
danielanputri <danielanputri@gmail.com>
2024-07-25 22:20:50 +0700

    Program Sederhana Kalkulator BMI

--- kalkulator_bmi.py
+++ kalkulator_bmi.py
@@ -3,10 +3,10 @@
 print ("PERHITUNGAN BMI (BODY MASS INDEX)")
 print("----------------------------------")
 
-berat_badan = input("Masukkan Berat Badan Anda (Kilogram): ") # ini hasilnya string
-berat_badan = float(berat_badan) #konversi dari string ke float
-tinggi_badan = input("Masukkan Tinggi Badan Anda (Meter): ") # ini hasilnya string
-tinggi_badan = float(tinggi_badan) #konversi dari string ke float
+berat_badan = float(input("Masukkan Berat Badan Anda (Kilogram): ")) # ini hasilnya string, lebih ringkas koversinya kalau begini
+# berat_badan = float(berat_badan) #konversi dari string ke float
+tinggi_badan = float(input("Masukkan Tinggi Badan Anda (Meter): ")) # ini hasilnya string, lebih ringkas koversinya kalau begini
+# tinggi_badan = float(tinggi_badan) #konversi dari string ke float
 
 bmi = berat_badan/(tinggi_badan**2)
 
@@ -14,12 +14,19 @@ berat_badan_ideal = dict()
 berat_badan_ideal['bawah'] = 18.5*(tinggi_badan**2)
 berat_badan_ideal['atas'] = 25*(tinggi_badan**2)
 
-# print(berat_badan_ideal)
+if bmi < 18.5:
+    kategori = ('Anda Kekurangan Berat Badan')
+elif bmi < 25:
+    kategori = ('Nilai BMI Anda Normal')
+elif bmi < 30:
+    kategori = ('Anda Kelebihan Berat Badan')
+else:
+    kategori = ('Anda Mengalami Obesitas')
 
-print(f"Nilai BMI Anda Adalah {bmi:.2f} kg/m^2")
-print("Nilai BMI normal anda adalah 18.5 kg/m^2 - 25 kg/m^2")
+print(f"\nNilai BMI Anda Adalah {bmi:.2f} kg/m^2")
+print(kategori)
 print(f"Berat Badan Ideal Anda Adalah Dalam Rentang "
       f"{berat_badan_ideal['bawah']:.2f}KG - {berat_badan_ideal['atas']:.2f}KG")
 
-print("---------------------------------------------")
+print("\n---------------------------------------------")
 print("Terima Kasih Sudah Menggunakan Program Ini :3")
